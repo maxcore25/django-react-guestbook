8aefdfd2514ca613872bdcb264218d904a5435e4
maxcore25 <mkorobov2002@mail.ru>
2022-02-15 09:09:12 +0300

    API URL Router in urls.py registered

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from notes.views import NotesViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('posts', NotesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
