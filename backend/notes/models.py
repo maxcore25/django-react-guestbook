a4097d723f1bb6fe27b8cbf8e5473ceb82ea121f
maxcore25 <mkorobov2002@mail.ru>
2022-02-15 07:53:03 +0300

    "Note" Class initialized

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+'''
+class Note:
+    title str
+    content str
+    date_created datetime
+'''
+
+
+class Note:
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
