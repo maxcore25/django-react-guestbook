05dc83ed4478a826789463d5cd91f14e71d9e3c2
maxcore25 <mkorobov2002@mail.ru>
2022-02-15 07:59:25 +0300

    "Note" Class Representation modified

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -12,3 +12,6 @@ class Note:
     title = models.CharField(max_length=255)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
