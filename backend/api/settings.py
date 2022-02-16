036557b2fc2dc425589f0426a7cef38902e064eb
maxcore25 <mkorobov2002@mail.ru>
2022-02-16 05:55:43 +0300

    CORS Allowed Origins added

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -52,6 +52,11 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
